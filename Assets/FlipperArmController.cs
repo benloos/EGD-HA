65d6b63825c0649b03f377b06b34b6bc7f894617
Alej-Smg <alexschmeing@web.de>
2021-06-07 18:37:58 +0200

    Flipper Arme hinzugefügt
    Flipper Arme Funktionieren soweit.

---
+++ Assets/FlipperArmController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperArmController : MonoBehaviour
+{
+   public float restPos = 0f;
+   public float pressedPos = 45f;
+   public float hitStrength = 100f;
+   public float damper = 150f;
+   public HingeJoint hinge;
+ 
+    void Start(){
+    }
+
+    void Update() {
+        JointSpring spring = new JointSpring();
+        spring.spring = this.hitStrength;
+        spring.damper = this.damper;
+        
+        if(Input.GetKey(KeyCode.A))
+            spring.targetPosition = pressedPos;
+        else
+            spring.targetPosition = restPos;
+        
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+}
